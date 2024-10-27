097938165b672a1bf3568857198afb9d08a227cc
Angello <agomezmonsalveangello@cesde.net>
2024-10-26 19:28:43 -0500

    Initial commit

---
+++ src/components/HeaderContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeaderContainer() {
+  return (
+    <header>
+        CODIGO DE LOGICA PARA DEFINIR SI 1 BOTON O 2
+    </header>
+  )
+}
+
+export default HeaderContainer
---
+++ src/components/ImageContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ImageContainer() {
+  return (
+    <div>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default ImageContainer
